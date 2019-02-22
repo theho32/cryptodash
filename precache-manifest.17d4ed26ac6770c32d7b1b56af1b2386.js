4cd7e59df5455173dbf53abbc1926209e4baedd3
Brandon Ho <brandonho5@gmail.com>
2019-02-22 10:56:33 -0800

    Updates

---
+++ precache-manifest.17d4ed26ac6770c32d7b1b56af1b2386.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "014249751641c05f49f9",
+    "url": "/cryptodash/static/js/runtime~main.01424975.js"
+  },
+  {
+    "revision": "39088686db621477dd69",
+    "url": "/cryptodash/static/js/main.39088686.chunk.js"
+  },
+  {
+    "revision": "e278741a5e12b80a64d7",
+    "url": "/cryptodash/static/js/2.e278741a.chunk.js"
+  },
+  {
+    "revision": "39088686db621477dd69",
+    "url": "/cryptodash/static/css/main.be03037c.chunk.css"
+  },
+  {
+    "revision": "14b3b04046f8caaa4062e000cf694d60",
+    "url": "/cryptodash/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cryptodash-pages/precache-manifest.b53cd71e96853fb6b1885a29aeb6808d.js"
+  "/cryptodash/precache-manifest.17d4ed26ac6770c32d7b1b56af1b2386.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cryptodash-pages/index.html", {
+workbox.routing.registerNavigationRoute("/cryptodash/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
